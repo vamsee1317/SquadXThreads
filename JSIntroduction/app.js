41e517a5ad3d1f54f8376a154712b99afdeffb20
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-07-31 11:05:08 +0530

    Js Intro | Js Implementation

---
+++ JSIntroduction/app.js
@@ -0,0 +1,43 @@
+// Agenda :
+
+// What is Javascript ?
+    // Javascript is high level programming or scripting language
+    // It is used for client side scripting
+    // It is used for server side scripting
+    // It is used for mobile and desktop application development
+    // It is used for game development
+    // It is a interpreted language
+    // // It is a dynamically typed language
+
+    // Js is high level programming and interpreted language used for creating interactive websites and also helps in adding functionalities.
+
+
+// History and Evolution of JS :
+    // Javascript was created by Brendan Eich in 1995
+    // It was initially called Mocha
+    // It was later renamed to Javascript
+    // It was first implemented in Netscape Navigator 2.0
+    // It was later adopted by Microsoft and became a standard for web development
+    // It has undergone many changes and updates over the years
+    // It is now a widely used language for web development
+// ECMAScript:
+    // ECMAScript is the standard for Javascript
+
+// Why Js ?
+  // Js is used for buildind end to end applications (LinkedIn, Facebook, Google, Netflix...etc).
+
+
+// What is Node.js ?
+    // Node.js is a runtime environment for Javascript.
+
+
+// What is Javascript Engine ?
+    // Javascript engine is a program that parses and executes Javascript code.
+
+    // V8 - Google Chrome
+    // SpiderMonkey - Mozilla Firefox
+    // JavaScriptCore - Safari
+    // Chakra - Microsoft Edge
+    
+// What is V8 Engine ?
+    // V8 Engine is a Javascript engine developed by Google.
---
+++ JsImplementation/app.js
@@ -0,0 +1,6 @@
+document.writeln("External Js");
+
+
+const inlineJsListItem = document.getElementById("inlineJs");
+
+inlineJsListItem.innerText = "Inline Javascript";
